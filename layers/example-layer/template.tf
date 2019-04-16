35760695fd33b1951f5eb4ebf81bafe47b2e470e
Luke Meyrick <luke@meyrick.eu>
2019-04-16 21:37:17 +0100

    Template errors in syntax

--- layers/example-layer/template.tf
+++ layers/example-layer/template.tf
@@ -15,12 +15,11 @@ resource "google_compute_instance_template" "primary-instance-template" {
 
   network_interface {
     network = "default"
-  }
-
-  access_config {
-    # The presence of this property assigns a public IP address to each Compute Instance. It is intentionally left
-    # blank so that an external IP address is selected automatically.
-    nat_ip = ""
+    access_config {
+      # The presence of this property assigns a public IP address to each Compute Instance. It is intentionally left
+      # blank so that an external IP address is selected automatically.
+      nat_ip = ""
+    }
   }
 
   disk {
